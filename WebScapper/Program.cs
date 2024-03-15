95a2d8e933e21890a7095358d8def83fcee177f8
iaguuu <iagooliveiraviana@gmail.com>
2024-03-15 01:07:37 -0300

    start project

---
+++ WebScapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
